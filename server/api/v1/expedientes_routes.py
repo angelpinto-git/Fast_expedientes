89750a1961ac29c95e5d50f29ddb28d088c04125
angelpinto-git <angelpinto9698@gmail.com>
2024-11-17 18:22:48 -0300

    Agrega body params para los endpoints de post y patch y el schema para las respuestas

--- server/api/v1/expedientes_routes.py
+++ server/api/v1/expedientes_routes.py
@@ -1,6 +1,8 @@
-from typing import Annotated
+from typing import Annotated, List
 
 from fastapi import APIRouter, Path, Query
+
+from server.schemas.expedientes_schemas import NewRecordRequest, RecordResponse, RecordRequest
 
 router = APIRouter(prefix='/expedientes')
 
@@ -12,7 +14,7 @@ router = APIRouter(prefix='/expedientes')
     },
     description='Retorna una lista paginada con los expedientes. Si no hay expedientes para mostrar, retorna lista vacía.'
     )  # POST /expedientes
-async def get_list(limit: Annotated[int, Query(ge=1, le=1000)] = 10, offset: Annotated[int, Query(ge=1, le=1000)] = 0) -> list:
+async def get_list(limit: Annotated[int, Query(ge=1, le=1000)] = 10, offset: Annotated[int, Query(ge=0)] = 0) -> List[RecordResponse]:
     print(f'Paginado limite {limit} y offset {offset}')
     return []
 
@@ -24,19 +26,20 @@ async def get_list(limit: Annotated[int, Query(ge=1, le=1000)] = 10, offset: Ann
     },
     description='Crea un expediente nuevo con los campos pasado por Body Param. Falla si faltan alguno de los campos obligatorios.'
     )  # POST /expedientes
-async def create() -> dict:
-    return {}
+async def create(new_record: NewRecordRequest) -> RecordResponse:
+    # Recibir un objeto
+    return new_record
 
 @router.get(
     '/{ID}',
     status_code=200,
     responses={
-        200: {'description': 'Expediente actualizado'},
+        200: {'description': 'Expediente encontrado'},
         422:{'description': 'ID no es un entero válido'}
     },
     description='Retorna un expediente por ID. Falla si el ID no existe.'
     )  # GET /expedientes/{ID}
-async def get_by_ID(id: Annotated[int, Path(ge=1)]) -> dict:
+async def get_by_ID(id: Annotated[int, Path(ge=1)]) -> RecordResponse:
     return {'id': id}
 
 @router.patch(
@@ -48,7 +51,7 @@ async def get_by_ID(id: Annotated[int, Path(ge=1)]) -> dict:
     },
     description='Actualiza un expediente con la data de Body Param. Falla si el ID no existe.'
     )  # PATCH /expedientes/{ID}
-async def updaate(id: Annotated[int, Path(ge=1)]) -> dict:
+async def updaate(id: Annotated[int, Path(ge=1)], record: RecordRequest) -> RecordResponse:
     return {'id': id}
 
 @router.delete(
---
+++ server/schemas/expedientes_schemas.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class NewRecordRequest(BaseModel) :
+    title: str
+    status: str = 'new'
+    description: str = ''
+
+class RecordRequest(BaseModel) :
+    title: str | None = None
+    status: str | None = None 
+    description: str | None = None
+
+class RecordResponse(BaseModel) :
+    id: int
+    title: str
+    status: str = 'new'
+    description: str = ''
+    created_at: datetime
+    updated_at: datetime
+    
